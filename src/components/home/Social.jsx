b4a46acfccf72977ed7ef45a110758d8ea528a10
Caleb Siegel <caleb.siegel@gmail.com>
2024-09-12 15:50:16 -0400

    build home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="http://www.linkedin.com/in/calebsiegel" className="home__social-icon" target="_blank">
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/caleb-siegel" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://www.facebook.com/caleb.siegel.5" className="home__social-icon" target="_blank">
+            <i class="uil uil-facebook"></i>
+        </a>
+
+        <a href="https://www.instagram.com/kbsiegel/" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+
+        {/* <a href="https://medium.com/@caleb.siegel" className="home__social-icon" target="_blank">
+            <i class="uil uil-medium-alt"></i>
+        </a> */}
+
+        <a href="https://www.youtube.com/@calebsiegel1" className="home__social-icon" target="_blank">
+            <i class="uil uil-youtube"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
