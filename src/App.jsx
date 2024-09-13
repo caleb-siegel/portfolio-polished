fa0fc22be7d453d2b204c947fc75868d27e53b3c
Caleb Siegel <caleb.siegel@gmail.com>
2024-09-13 11:14:57 -0400

    build scrollup component + activenav

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Skills from './components/skills/Skills'
 import Qualification from './components/qualification/Qualification'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import ScrollUp from './components/scrollup/ScrollUp'
 
 function App() {
   return (
@@ -19,8 +20,11 @@ function App() {
         <Skills />
         <Qualification />
         <Contact />
-        <Footer />
+        
       </main>
+
+      <Footer />
+      <ScrollUp />
     </>
   )
 }
