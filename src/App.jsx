051eb45699507ea5f5a8ca25f46fd34920127699
Caleb Siegel <caleb.siegel@gmail.com>
2024-09-16 12:47:50 -0400

    hopefully adding vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Footer from './components/footer/Footer';
 import ScrollUp from './components/scrollup/ScrollUp';
 import Work from './components/work/Work';
 import Interests from './components/interests/Interests';
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -29,7 +28,6 @@ function App() {
 
       <Footer />
       <ScrollUp />
-      <Analytics />
     </>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -58,12 +58,12 @@ const Header = () => {
                         </li>
                     </ul>
 
-                    <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
+                    <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
                 </div>
 
                 {Toggle ? "" : 
                     <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-                        <i class="uil uil-bars"></i>
+                        <i className="uil uil-bars"></i>
                     </div>
                 }
             </nav>
