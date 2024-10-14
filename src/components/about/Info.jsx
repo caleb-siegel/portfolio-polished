2d0d6f019532e8a90e5f19dfb211367d8f2bf8bf
Caleb Siegel <caleb.siegel@gmail.com>
2024-10-14 14:43:51 -0400

    change git commits to over 400

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -12,7 +12,7 @@ const Info = () => {
         <div className="about__box">
             <i class='bx bx-briefcase-alt about__icon' ></i>
             <h3 className="about__title">Completed</h3>
-            <span className="about__subtitle">300+ Github Contributions</span>
+            <span className="about__subtitle">400+ Github Contributions</span>
         </div>
 
         <div className="about__box">
