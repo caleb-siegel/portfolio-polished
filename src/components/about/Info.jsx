9c5a94991061a38fbccdf9ac1784984662ada854
Caleb Siegel <caleb.siegel@gmail.com>
2025-04-15 14:23:16 +0300

    change swe to engineer

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -7,7 +7,7 @@ const Info = () => {
             <i class='bx bx-award about__icon'></i>
             <h3 className="about__title">Experience</h3>
             <span className="about__subtitle">7 Years Business</span>
-            <span className="about__subtitle">1 Year SWE</span>
+            <span className="about__subtitle">1 Year Engineer</span>
         </div>
 
         <div className="about__box">
